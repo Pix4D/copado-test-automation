77a4c96be0407f4f314afa9f2fab0ff68f2503ae
Ali Mengütay <ali.mengutay@pix4d.com>
2024-02-28 14:55:21 +0100

    cookies not working still

--- Libraries/CookieManager.py
+++ Libraries/CookieManager.py
@@ -4,13 +4,13 @@ class CookieManager:
     def __init__(self):
         self.selenium_lib = BuiltIn().get_library_instance('SeleniumLibrary')
 
-    def transfer_cookies(self, source_url, target_url):
-        self.selenium_lib.open_browser(source_url, browser='chrome')
+    def transfer_cookies(self, target_url):
+        # Assuming the browser is already opened and at the source_url with the desired cookies set
         cookies = self.selenium_lib.get_cookies()
         self.selenium_lib.go_to(target_url)
-       
-
- for cookie in cookies:
+        for cookie in cookies:
+            # Before adding cookies, make sure the browser is on the domain of the cookie.
+            # Some browsers may restrict adding cookies if the current domain doesn't match the cookie domain.
             self.selenium_lib.add_cookie(cookie['name'], cookie['value'])
-        # Optionally, after setting cookies, you might want to refresh the page to ensure the cookies take effect.
+        # Refresh the page to ensure the cookies take effect.
         self.selenium_lib.reload_page()
