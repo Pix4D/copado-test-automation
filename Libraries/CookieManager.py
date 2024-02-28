b761cb34562456d3e6e99e2621bd0f71f47a3c8e
Ali Mengütay <ali.mengutay@pix4d.com>
2024-02-28 16:51:28 +0100

    cookies not working still

--- Libraries/CookieManager.py
+++ Libraries/CookieManager.py
@@ -4,13 +4,19 @@ class CookieManager:
     def __init__(self):
         self.selenium_lib = BuiltIn().get_library_instance('SeleniumLibrary')
 
-    def transfer_cookies(self, target_url):
-        # Assuming the browser is already opened and at the source_url with the desired cookies set
+    def store_cookies(self):
+        # Store cookies from the current session
         cookies = self.selenium_lib.get_cookies()
+        return cookies
+
+    def reapply_cookies(self, cookies, target_url):
+        # Navigate to the target URL
         self.selenium_lib.go_to(target_url)
+        # Reapply stored cookies
         for cookie in cookies:
-            # Before adding cookies, make sure the browser is on the domain of the cookie.
-            # Some browsers may restrict adding cookies if the current domain doesn't match the cookie domain.
-            self.selenium_lib.add_cookie(cookie['name'], cookie['value'])
-        # Refresh the page to ensure the cookies take effect.
+            if 'domain' in cookie and 'expiry' in cookie:  # Check if 'domain' and 'expiry' are in the cookie dict to avoid errors
+                self.selenium_lib.add_cookie(cookie['name'], cookie['value'], path=cookie.get('path', '/'), domain=cookie['domain'], secure=cookie.get('secure', False), expiry=cookie['expiry'])
+            else:
+                self.selenium_lib.add_cookie(cookie['name'], cookie['value'])
+        # Refresh the page to ensure cookies are applied
         self.selenium_lib.reload_page()
